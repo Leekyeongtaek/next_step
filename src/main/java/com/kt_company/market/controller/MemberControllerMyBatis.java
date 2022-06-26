2c2c2b8136d3a32dc89cf79cc38aa689e36723e8
kt_home <mrlee@mncnext.co.kr>
2022-06-26 21:15:35 +0900

    로그인 기능 추가.

--- src/main/java/com/kt_company/market/controller/MemberControllerMyBatis.java
+++ src/main/java/com/kt_company/market/controller/MemberControllerMyBatis.java
@@ -1,35 +1,32 @@
 package com.kt_company.market.controller;
 
 
-import com.kt_company.market.mapper.MemberMapper;
+import com.kt_company.market.dto.MemberDto;
+import com.kt_company.market.service.MemberServiceBatis;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/member/batis")
 @RestController
-@Controller
 public class MemberControllerMyBatis {
 
-    // 기능 요구 사항.
-    /**
-     * 1. Api. 만들기. -> Service 단 호출.
-     * 2. 멤버 조회하기. -> 매퍼 써서.
-     */
+    private final MemberServiceBatis memberServiceBatis;
 
-    private final MemberMapper memberMapper;
-
-    
-//    @PostMapping("/find")
-//    public
-
-
-
+    @GetMapping("/")
+    public ResponseEntity<?> login(@ModelAttribute MemberDto memberDto) throws Exception {
+        try {
+            MemberDto findMember = memberServiceBatis.login(memberDto);
+            return new ResponseEntity<>(findMember.getId(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
---
+++ src/main/java/com/kt_company/market/service/MemberServiceBatis.java
@@ -0,0 +1,33 @@
+package com.kt_company.market.service;
+
+import com.kt_company.market.dto.MemberDto;
+import com.kt_company.market.mapper.MemberMapper;
+import com.kt_company.market.util.common.StringUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class MemberServiceBatis {
+
+    private final MemberMapper memberMapper;
+
+    /**
+     * 로그인.
+     */
+    public MemberDto login(MemberDto dto) throws Exception {
+        try{
+            dto.setPassword(StringUtil.convertSHA256(dto.getPassword()));
+            MemberDto findMember = memberMapper.selectMember(dto);
+            if(findMember == null) {
+                throw new Exception("존재하지 않은 아이디 입니다.");
+            }
+            return findMember;
+        }catch (Exception e){
+            throw new Exception(e);
+        }
+    }
+
+}
