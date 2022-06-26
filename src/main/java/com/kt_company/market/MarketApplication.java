5d9d70ca8fe7269c930eaa054fde642c46920c8c
kt_home <mrlee@mncnext.co.kr>
2022-06-26 21:56:05 +0900

    mybatis 연동.

--- src/main/java/com/kt_company/market/MarketApplication.java
+++ src/main/java/com/kt_company/market/MarketApplication.java
@@ -2,6 +2,7 @@ package com.kt_company.market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class MarketApplication {
---
+++ src/main/java/com/kt_company/market/config/DatasourceConfig.java
@@ -0,0 +1,53 @@
+package com.kt_company.market.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Slf4j
+@Configuration
+@MapperScan(basePackages = "com.kt_company.market.mapper")
+@PropertySource("classpath:/application.yml")
+public class DatasourceConfig {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public HikariConfig hikariConfig() {
+        // application.yml 에 있는 spring.datasource 를 읽어 HikariConfig에 관련된 설정을 해준다.
+        return new HikariConfig();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DataSource dataSource = new HikariDataSource(hikariConfig());
+        return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/mappers/**/*.xml"));
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+}
