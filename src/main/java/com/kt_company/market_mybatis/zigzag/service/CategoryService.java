e97ad3be697cd39f9fd264a8598aef127616265a
kt_home <mrlee@mncnext.co.kr>
2022-07-02 09:31:48 +0900

    지그재그 카테고리 조회.

--- src/main/java/com/kt_company/market_mybatis/zigzag/service/CategoryService.java
+++ src/main/java/com/kt_company/market_mybatis/zigzag/service/CategoryService.java
@@ -1,9 +1,14 @@
 package com.kt_company.market_mybatis.zigzag.service;
 
+import com.kt_company.market_mybatis.zigzag.util.HttpClientZigzag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class CategoryService {
+
+    private final HttpClientZigzag httpClientZigzag;
 
     private final String accessKey = "69a35e30-e2f4-4aa0-88df-6f6dae10eb7e";
     private final String secretKey = "8034d4ad0118d92daa8cdb80cc0265654db2db8e";
@@ -15,6 +20,12 @@ public class CategoryService {
         + "{ id name } "
         + "} } } }";
 
+    /**
+     * 지그재그 카테고리 조회.
+     */
+    public void getZigzagCategory () {
+//        httpClientZigzag.post();
+    }
 
 
 }
---
+++ src/test/java/com/kt_company/market_mybatis/zigzag/service/CategoryServiceTest.java
@@ -0,0 +1,28 @@
+package com.kt_company.market_mybatis.zigzag.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class CategoryServiceTest {
+
+//    @Autowired
+//    CategoryService categoryService;
+
+
+    @Test
+    @DisplayName("지그재그 카테고리 조회.")
+    public void getCategory() {
+
+    }
+
+
+}
