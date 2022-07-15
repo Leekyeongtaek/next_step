326250d8406648a9d06d8cf54c2b8f0c9c565b1f
kt_home <mrlee@mncnext.co.kr>
2022-07-15 10:46:13 +0900

    회원 데이터 입력 테스트 코드 작성중.

--- src/test/java/com/kt_company/market/service/MemberServiceTest.java
+++ src/test/java/com/kt_company/market/service/MemberServiceTest.java
@@ -1,16 +1,35 @@
 package com.kt_company.market.service;
 
+import com.kt_company.market.dto.MemberDto;
+import com.kt_company.market.util.common.StringUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @Slf4j
 @SpringBootTest
 public class MemberServiceTest {
 
+    @Autowired
+
+
     @Test
     public void init() {
         log.debug("초기화.....");
+    }
+
+    @Test
+    @DisplayName("테스트용 회원 데이터 입력.")
+    public void joinMembers() {
+
+        for (int i = 0; i < 1000; i++) {
+            MemberDto memberDto = new MemberDto();
+//            memberDto.setPassword();
+//            StringUtil.convertSHA256(memberDto.getPassword()//
+        }
+
     }
 
 
